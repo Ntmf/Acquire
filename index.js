ef0958afbd715b3a011669f2b9392b2bac2a7e93
Rob Lucas <492168+Ntmf@users.noreply.github.com>
2022-01-29 16:52:58 -0700

    update express paths

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  //.set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'html')
+  //.get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('AquireApp'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
