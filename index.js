425dcc861ed7a465137b959c525079b0a62665b0
Rob Lucas <492168+Ntmf@users.noreply.github.com>
2022-02-01 18:49:50 -0700

    Remove express settings

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('AcquireApp.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080); 
+
+/*
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -8,3 +19,4 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
