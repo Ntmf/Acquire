42cdc699fc9f94a0a554b30073a41ae0e28f4cad
Rob Lucas <492168+Ntmf@users.noreply.github.com>
2022-02-01 18:54:24 -0700

    update port to listen on

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var http = require('http');
 var fs = require('fs');
+const PORT = process.env.PORT || 5000
 http.createServer(function (req, res) {
   fs.readFile('AcquireApp.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
   });
-}).listen(8080); 
+}).listen(PORT); 
 
 /*
 const express = require('express')
