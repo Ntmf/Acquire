d99a7a83917fe9db699e5b26c03460581310d870
Rob Lucas <492168+Ntmf@users.noreply.github.com>
2022-02-01 19:31:49 -0700

    trying express again...

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-var http = require('http');
-var fs = require('fs');
-const PORT = process.env.PORT || 5000
-http.createServer(function (req, res) {
-  fs.readFile('AcquireApp.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(PORT); 
 
-/*
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -18,6 +7,5 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/AcquireApp'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-*/
